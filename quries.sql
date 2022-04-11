58232976af022c5ad67f60d4b1df1e8656815d88
Dewashish Tarale <76059448+dewashisht@users.noreply.github.com>
2022-04-11 10:40:38 +0530

    Update quries.sql

--- quries.sql
+++ quries.sql
@@ -1,5 +1,5 @@
 select count(*) AS taxi from taxi;
-b)Find driver name name from cargo table whose name exists ‘gis’ or ‘GIS’ as a substring:
+b)Find driver name name from taxi table whose name exists ‘gis’ or ‘GIS’ as a substring:
 select taxi_number from taxi where  taxi_number LIKE '%gis%' OR  taxi_number LIKE '%GIS%';
 c)Find details of all driver by sorting their name in descending order:
-select * from driver ORDER BY driver_name DESC;
+select * from driver booked BY driver_name DESC;
